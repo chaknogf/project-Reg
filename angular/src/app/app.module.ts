951c36be74436f9c0eef661a0fea59e72736af1d
chaknogf <chaknogf@gmail.com>
2023-05-02 06:16:29 -0600

    bootstrap

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PacientesComponent } from './pacientes/pacientes.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PacientesComponent
+    PacientesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
